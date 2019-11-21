81f6300b089cf653345ae8e42aeef88885cd5e04
ttingen <ttingen@gmail.com>
2019-11-20 20:15:35 -0500

    Beginnings of the LeasedDatabaseAsync class.

---
+++ src/StackExchange.Redis/Interfaces/ILeasedDatabaseAsync.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StackExchange.Redis.Interfaces
+{
+    /// <summary>
+    /// Describes functionality for blocking operations.
+    /// </summary>
+    public interface ILeasedDatabaseAsync : IDatabaseAsync
+    {
+        /// <summary>
+        /// Removes and returns the first element of the list stored at key(s).
+        /// </summary>
+        /// <param name="keys">One or more list keys.</param>
+        /// <param name="timeout">The time to wait for items in the list(s).</param>
+        /// <param name="flags">The flags to use for this operation.</param>
+        /// <returns>KeyValuePair for each list that returned an item.</returns>
+        /// <remarks>https://redis.io/commands/blpop</remarks>
+        Task<KeyValuePair<string, string>[]> ListLeftPopAsync(RedisKey[] keys, TimeSpan timeout, CommandFlags flags = CommandFlags.None);
+    }
+}
---
+++ src/StackExchange.Redis/LeasedDatabaseAsync.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis.Interfaces;
+
+namespace StackExchange.Redis
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class LeasedDatabaseAsync : RedisDatabase, ILeasedDatabaseAsync, IDatabaseAsync
+    {
+        internal LeasedDatabaseAsync(ConnectionMultiplexer multiplexer, int db, object asyncState)
+            : base(multiplexer, db, asyncState)
+        { }
+
+        public Task<KeyValuePair<string, string>[]> ListLeftPopAsync(RedisKey[] keys, TimeSpan timeout, CommandFlags flags = CommandFlags.None)
+        {
+            if (keys == null) throw new NullReferenceException(nameof(keys));
+            if (keys.Length == 0) throw new ArgumentException("Parameter 'keys' must contain at least one RedisKey.");
+
+            ThrowIfInvalidTimeout(timeout);
+
+            var values = new RedisValue[keys.Length + 1];
+            var offset = 0;
+
+            foreach (var key in keys)
+            {
+                values[offset++] = key.AsRedisValue();
+            }
+
+            values[offset] = timeout.TotalSeconds;
+
+            // TODO: mark the message blocking so it knows to get a dedicated connection to run the operation on.
+
+            var msg = Message.Create(Database, flags, RedisCommand.BLPOP, values);
+            return ExecuteAsync(msg, ResultProcessor.StringPairInterleaved);
+        }
+
+        private void ThrowIfInvalidTimeout(TimeSpan timeout)
+        {
+            if (timeout.TotalSeconds == 0) throw new ArgumentException("Invalid timeout, the total seconds must be greater than 0.");
+        }
+    }
+}
